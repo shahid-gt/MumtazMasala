c033bf90813a6b1c454eadb091b60f6338219beb
shahid.shaikh <shahid.shaikh@google.com>
2023-12-02 22:07:37 +0530

    1: comment out and add some code in index.php file

--- index.php
+++ index.php
@@ -5,32 +5,33 @@ $uri = explode('/', $_SERVER['REQUEST_URI']);
 $web['uri'] = $uri;
 $web['breadcrumb'] = '';
 $web['page'] = '';
+$web['host'] = '/MumtazMasala/';
 $root = true;
 
-if ( isset($uri[1]) && $uri[1] != '' && $uri[1] != 'index.php' )
-{
-	$root = false;
-	// Categories
-	$sql = "SELECT id FROM ".$app->getPrefix()."categories WHERE slug = ?";
-	$params = array($uri[1]);
-	$rws = $app->getList($sql, $params);
-	if ( !empty($rws) )
-	{
-		if ( isset($uri[2]) )
-		{
-			$params = str_replace('?', '', $uri[2]);
-			$arys = explode('&', $params);
-			foreach( $arys as $ary )
-			{
-				$tmpary = explode('=', $ary);
-				$request->set($tmpary[0], $tmpary[1]);
-			}
-		}
-		$request->set('category_id', $rws[0]['id']);
-		require('products.php');
-		exit;
-	}
-}
+// if ( isset($uri[1]) && $uri[1] != '' && $uri[1] != 'index.php' )
+// {
+// 	$root = false;
+// 	// Categories
+// 	$sql = "SELECT id FROM ".$app->getPrefix()."categories WHERE slug = ?";
+// 	$params = array($uri[1]);
+// 	$rws = $app->getList($sql, $params);
+// 	if ( !empty($rws) )
+// 	{
+// 		if ( isset($uri[2]) )
+// 		{
+// 			$params = str_replace('?', '', $uri[2]);
+// 			$arys = explode('&', $params);
+// 			foreach( $arys as $ary )
+// 			{
+// 				$tmpary = explode('=', $ary);
+// 				$request->set($tmpary[0], $tmpary[1]);
+// 			}
+// 		}
+// 		$request->set('category_id', $rws[0]['id']);
+// 		require('products.php');
+// 		exit;
+// 	}
+// }
 
 if ( $root )
 	require('home.php');

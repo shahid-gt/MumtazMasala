04735fa6eb00f0830686f17ac5af2bda8130ca77
shahid.shaikh <shahid.shaikh@google.com>
2023-12-02 21:51:14 +0530

    Updating config and index

--- index.php
+++ index.php
@@ -7,30 +7,30 @@ $web['breadcrumb'] = '';
 $web['page'] = '';
 $root = true;
 
-// if ( isset($uri[1]) && $uri[1] != '' && $uri[1] != 'index.php' )
-// {
-// 	$root = false;
-// 	// Categories
-// 	$sql = "SELECT id FROM ".$app->getPrefix()."categories WHERE slug = ?";
-// 	$params = array($uri[1]);
-// 	$rws = $app->getList($sql, $params);
-// 	if ( !empty($rws) )
-// 	{
-// 		if ( isset($uri[2]) )
-// 		{
-// 			$params = str_replace('?', '', $uri[2]);
-// 			$arys = explode('&', $params);
-// 			foreach( $arys as $ary )
-// 			{
-// 				$tmpary = explode('=', $ary);
-// 				$request->set($tmpary[0], $tmpary[1]);
-// 			}
-// 		}
-// 		$request->set('category_id', $rws[0]['id']);
-// 		require('products.php');
-// 		exit;
-// 	}
-// }
+if ( isset($uri[1]) && $uri[1] != '' && $uri[1] != 'index.php' )
+{
+	$root = false;
+	// Categories
+	$sql = "SELECT id FROM ".$app->getPrefix()."categories WHERE slug = ?";
+	$params = array($uri[1]);
+	$rws = $app->getList($sql, $params);
+	if ( !empty($rws) )
+	{
+		if ( isset($uri[2]) )
+		{
+			$params = str_replace('?', '', $uri[2]);
+			$arys = explode('&', $params);
+			foreach( $arys as $ary )
+			{
+				$tmpary = explode('=', $ary);
+				$request->set($tmpary[0], $tmpary[1]);
+			}
+		}
+		$request->set('category_id', $rws[0]['id']);
+		require('products.php');
+		exit;
+	}
+}
 
 if ( $root )
 	require('home.php');
